659a33fb9189d2ee0e81cb12977b988fd7bbdb77
dzuberi <dzuberi522@gmail.com>
2026-04-19 14:12:49 -0700

    add assertions for pipewire and wireplumber

--- flake.nix
+++ flake.nix
@@ -11,45 +11,59 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+    packages = forAllSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
         {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "btd700ctl";
-            version = "unstable-${self.shortRev or "dirty"}";
+        default = pkgs.stdenv.mkDerivation {
+          pname = "btd700ctl";
+          version = "unstable-${self.shortRev or "dirty"}";
 
-            src = ./.;
+          src = ./.;
 
-            nativeBuildInputs = with pkgs; [ cmake pkg-config makeWrapper ];
-            buildInputs = with pkgs; [ hidapi ];
+          nativeBuildInputs = with pkgs; [ cmake pkg-config makeWrapper ];
+          buildInputs = with pkgs; [ hidapi ];
 
-            postInstall = ''
+          postInstall = ''
               install -Dm644 ../udev/99-btd700.rules -t $out/lib/udev/rules.d/
               substituteInPlace $out/lib/systemd/user/btd700d.service \
-                --replace-fail "/usr/local/bin/btd700d" "$out/bin/btd700d"
+              --replace-fail "/usr/local/bin/btd700d" "$out/bin/btd700d"
               wrapProgram $out/bin/btd700d \
-                              --prefix PATH : ${pkgs.wireplumber}/bin
-            '';
+              --prefix PATH : ${pkgs.wireplumber}/bin
+              '';
 
-            meta = with pkgs.lib; {
-              description = "Unofficial linux driver for the Sennheiser BTD 700 dongle";
-              homepage = "https://github.com/sobalap/btd700ctl";
-              license = licenses.lgpl21Only;
-              platforms = platforms.linux;
-              mainProgram = "btd700ctl";
-            };
+          meta = with pkgs.lib; {
+            description = "Unofficial linux driver for the Sennheiser BTD 700 dongle";
+            homepage = "https://github.com/sobalap/btd700ctl";
+            license = licenses.lgpl21Only;
+            platforms = platforms.linux;
+            mainProgram = "btd700ctl";
           };
-        });
-
-      nixosModules.default = { pkgs, ... }: {
-        services.udev.packages = [ self.packages.${pkgs.system}.default ];
-        environment.systemPackages = [ self.packages.${pkgs.system}.default ];
-        systemd.packages = [ self.packages.${pkgs.system}.default ];
-        systemd.user.services.btd700d = {
-          wantedBy = [ "default.target" ];
         };
+      });
+    nixosModules.default = { pkgs, lib, config, ... }: 
+      let
+        pkg = self.packages.${pkgs.system}.default;
+      in {
+      assertions = [
+        {
+          assertion = config.services.pipewire.enable == true;
+          message = "The btd700d module requires 'services.pipewire.enable = true;'";
+        }
+        {
+          assertion = config.services.pipewire.wireplumber.enable == true;
+          message = "The btd700d module requires 'services.pipewire.wireplumber.enable = true;'";
+        }
+      ];
+
+      services.udev.packages = [ pkg ];
+      environment.systemPackages = [ pkg ];
+      systemd.packages = [ pkg ];
+
+      systemd.user.services.btd700d = {
+        wantedBy = [ "default.target" ];
+        enable = true;
       };
     };
-}
+  }
